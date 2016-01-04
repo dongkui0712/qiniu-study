6e9c941b3f7416a3acd7fce4c4d693e435d19530
hao3.zhou <hao3zhou@hao3zhoudeMacBook-Pro.local>
2016-01-04 21:15:25 +0800

    summary2016-01-04

--- zhouhao/20160104/main.go
+++ zhouhao/20160104/main.go
@@ -6,7 +6,7 @@ import (
 func main()  {
 	result,err:=division.Division(8,1)
 	if(err==nil){
-		fmt.Println("结果是",result)
+		fmt.Println("result is",result)
 	}else {
 		fmt.Println("出错鸟,错误原因是:",err)
 
