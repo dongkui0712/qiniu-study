e64fc6b0edc58d71d26fe7ee6484e6e9d56a5cd6
Apple_Xuan <zhongrong.kang@changhong.com>
2016-01-07 10:25:05 +0800

    commit prime

--- kangzr/test0106/calculateNumTest.go
+++ kangzr/test0106/calculateNumTest.go
@@ -2,78 +2,69 @@ package main
 
 import (
 	"fmt"
-	_ "sort"
 )
 
 func main() {
-	//	findPrimeNumber(2)
-
-	is, cIf, cFor := isPrime(3)
-	fmt.Println(is, cIf, cFor)
+	findPrimeNumber(100)
 }
 
-func findPrimeNumber(n int32) {
-	var intIf int32 = 0
-	var intFor int32 = 0
-	var primes []int32
-	var i int32
-	for i = 1; i < n; i++ {
-		is, cIf, cFor := isPrime(i)
+func findPrimeNumber(n int) {
+	var intIf int = 0
+	var primes []int
+	for i := 1; i < n; i++ {
+		is, cIf := isPrime(i)
 		intIf = intIf + cIf
-		intFor = intFor + cFor
 		if is == true {
+			intIf = intIf + cIf
 			primes = append(primes, i)
 		}
 	}
 
-	//	sort.Ints([]int(primes))
-	num := int32(len(primes) - 1)
-	fmt.Println("最大素数为:", primes[num])
+	num := int(len(primes) )
+	fmt.Println("num:",num)
+	fmt.Println("最大素数为:", primes[num-1])
 	fmt.Println("if执行次数:", intIf)
-	fmt.Println("for执行次数:", intFor)
 	//	for _,v := range primes {
 	//
 	//	}
-
 }
 
-func isPrime(value int32) (bool, int32, int32) {
-	var is bool
-	var int32If int32
-	var int32For int32
+func isPrime(value int) (is bool, intIf int) {
+	intIf=0
 	if value <= 3 {
 		is = (value >= 2)
-		int32If++
+		intIf++
+		return is, intIf
 	}
 
 	if value%2 == 0 || value%3 == 0 {
 		is = false
-		int32If++
+		intIf++
+		return is, intIf
 	}
 
-	var i int32
-	for i = 5; i*i <= value; i += 6 {
-		int32For++
-		if value%i == 0 || value%(i+2) == 0 {
-			is = false
-			int32If++
-		}
-	}
-
-	return is, int32If, int32For
-}
-
-func isPrimeB(value int) bool {
-	if value <= 3 {
-		return value >= 2
-	}
-	if value%2 == 0 || value%3 == 0 {
-		return false
-	}
 	for i := 5; i*i <= value; i += 6 {
+		intIf=intIf+1
 		if value%i == 0 || value%(i+2) == 0 {
-			return false
+			intIf=intIf+1
+			return false, intIf
 		}
 	}
-	return true
+
+	return true, intIf
 }
+
+//func isPrimeB(value int) bool {
+//	if value <= 3 {
+//		return value >= 2
+//	}
+//	if value%2 == 0 || value%3 == 0 {
+//		return false
+//	}
+//	for i := 5; i*i <= value; i += 6 {
+//		if value%i == 0 || value%(i+2) == 0 {
+//			return false
+//		}
+//	}
+//	return true
+//}
