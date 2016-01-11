64a87c5ce08adb103d7e71352ebf5169858c9990
Apple_Xuan <zhongrong.kang@changhong.com>
2016-01-11 15:07:01 +0800

    修改创业练习题

--- kangzr/test0107/company_coder.go
+++ kangzr/test0107/company_coder.go
@@ -16,6 +16,14 @@ func main() {
 	cm.Init()
 
 	totalMoney := 100.0 //单位：w
+	noOrderTime := 0 //连续未接到订单的时间统计
+	isHavaCoder :=false
+
+	var coder Coder //现成的程序员
+	coder = nil
+	basicSalary := float64(0) //程序员对应的基本工资
+
+	num := 0 //循环次数
 
 	for {
 		if totalMoney > 1000.0 || totalMoney < 0 {
@@ -23,28 +31,53 @@ func main() {
 			return
 		}
 
-		fmt.Println("新一轮工作开始")
+		num++
+		fmt.Println("新一轮工作开始~~~~~~~~第", num, "轮")
 		order, err := om.GetOrder()
 		if err != nil {
 			fmt.Println("没有拉到订单，休息一天后继续")
 			time.Sleep(time.Second * 1) //1秒模拟1天
 			totalMoney -= 0.1
+			noOrderTime +=1
 			continue
 		}
+		noOrderTime = 0
 
-		coder,basicSalary, err := cm.GetCoder()
-		if err != nil {
-			fmt.Println("没有找到程序员，退回订单后继续")
-			om.ReturnOrder(order)
-			totalMoney -= order.Money * 2
-			continue
-		}
-
-		software, err := coder.WriteCode(order)
-		if err != nil {
-			fmt.Println("开发出错，公司倒闭")
+		if noOrderTime >= 90 {
+			fmt.Println("连续3个月未拉倒订单，创业失败!")
 			return
 		}
+
+
+		if isHavaCoder == false {
+			fmt.Println("没有现成的程序员,需要从人才市场中去拉")
+			coderGet,basicSalaryGet, err := cm.GetCoder()
+			if err != nil {
+				fmt.Println("没有找到程序员，退回订单后继续")
+				om.ReturnOrder(order)
+				totalMoney -= order.Money * 2
+				continue
+			}
+			coder = coderGet
+			basicSalary = basicSalaryGet
+		}
+
+
+		fmt.Println("订单要求的完成时间为:", order.DeadLine)
+		software, err := coder.WriteCode(order)
+		if software.line > order.DeadLine {
+			fmt.Println("订单延期,退回订单,赔付业务方2倍的违约金,并支付程序员基本工资")
+			om.ReturnOrder(order)
+			totalMoney -= order.Money * 2
+
+			moneyToCoder := basicSalary
+			coder.GetMoney(moneyToCoder)
+			totalMoney -= basicSalary
+
+			fmt.Println("创业总资金剩余:", totalMoney)
+			continue
+		}
+
 
 		om.DeliverOrder(order, software)
 		totalMoney += order.Money
@@ -53,17 +86,21 @@ func main() {
 		coder.GetMoney(moneyToCoder)
 		totalMoney -= moneyToCoder
 
+		left := om.Consult() // 协商结果
+		isHavaCoder = left
+
 		fmt.Println("创业总资金剩余:", totalMoney)
 		fmt.Println("给程序员买盒饭闪了腰，休息两天\n")
 		totalMoney -= 0.1 * 2
 		time.Sleep(time.Second * 2) //1秒模拟1天
+		noOrderTime = 0
 	}
 }
 
 type Order struct {
 	Money    float64 //w
 	Name     string
-	DeadLine string
+	DeadLine int
 }
 
 type OrderMarket struct {
@@ -78,6 +115,7 @@ func (om *OrderMarket) Init() {
 		order := Order{
 			Money: float64(rand.Int()%10 + 10),
 			Name:  "order:" + strconv.Itoa(i),
+			DeadLine: int(rand.Int()%2 + 10),
 		}
 		orderPool = append(orderPool, order)
 	}
@@ -86,8 +124,9 @@ func (om *OrderMarket) Init() {
 }
 
 func (om *OrderMarket) GetOrder() (order Order, err error) {
-	order = om.orderPool[0]
-	om.orderPool = append(om.orderPool[:1], om.orderPool[:1]...)
+	index := rand.Int() % len(om.orderPool)
+	order = om.orderPool[index]
+//	om.orderPool = append(om.orderPool[:1], om.orderPool[:1]...)
 	fmt.Printf("get an order:%v money:%vw\n", order.Name, order.Money)
 	return
 }
@@ -99,6 +138,17 @@ func (om *OrderMarket) ReturnOrder(order Order) {
 
 func (om *OrderMarket) DeliverOrder(order Order, sf SoftWare) {
 	fmt.Println("deliver order success. software info:", sf.Name)
+}
+
+func (om *OrderMarket) Consult() (left bool) {
+	left = bool(rand.Intn(3)%3 == 0)
+	if left == true{
+		fmt.Println("老A与程序员协商的结果是:留下来")
+	} else {
+		fmt.Println("老A与程序员协商的结果是:滚蛋")
+	}
+
+	return
 }
 
 type CoderMarket struct {
@@ -133,6 +183,7 @@ func (cm *CoderMarket) GetCoder() (coder Coder,basicSalary float64, err error) {
 
 type SoftWare struct {
 	Name string
+	line int
 }
 
 type Coder interface {
@@ -145,10 +196,12 @@ type GoCoder struct {
 }
 
 func (gc *GoCoder) WriteCode(order Order) (sf SoftWare, err error) {
-
+	line := int(rand.Int()%2 + 10)
 	fmt.Printf("begin to write code for %v\n", order.Name)
 	time.Sleep(time.Second * 3)
 	fmt.Println("write code ok.")
+	fmt.Println("本次开发周期为:", line)
+	sf.line = line
 
 	sf.Name = fmt.Sprintf("hello, world! by %v", gc.Name)
 	return
@@ -177,9 +230,12 @@ func (d *Dentist) GetMoney(money float64) {
 }
 
 func (d *Dentist) WriteCode(order Order) (sf SoftWare, err error) {
+	line := int(rand.Int()%2 + 10)
 	fmt.Printf("i'm a dentist. begin to write code for %v\n", order.Name)
 	time.Sleep(time.Second * 3)
 	fmt.Println("write code ok.")
+	fmt.Println("本次开发周期为:", line)
+	sf.line = line
 
 	sf.Name = fmt.Sprintf("hello, world! by dentist:%v", d.Name)
 	return
